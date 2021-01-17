dbeb95488d22dc2570e748c4aa1b5cdd54d98f41
meshal <mmas3l222@gmail.com>
2021-01-18 02:47:58 +0300

    someChange

---
+++ js.js
@@ -0,0 +1,30 @@
+$(document).ready(function(){
+
+
+$(".a").animate({
+
+left:"0px"
+
+
+},2000)
+
+,
+$(".b").animate({
+
+    top:"0px"
+    
+    
+    },2000)
+,
+
+$(".c").animate({
+
+    right:"0px"
+    
+    
+    },2000);
+$(".drp").fadeToggle(6000);
+
+
+
+});
